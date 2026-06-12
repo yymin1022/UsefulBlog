d1ec77a64fb0720d4cf776d9613ab97d8cc9a84a
yymin1022 <yymin1022@gmail.com>
2026-06-13 01:10:20 +0900

    Update Project : Revert next.config.mjs env mapping

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        NEXT_PUBLIC_FB_API_KEY: process.env.FB_API_KEY || process.env.NEXT_PUBLIC_FB_API_KEY,
-        NEXT_PUBLIC_FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN || process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-        NEXT_PUBLIC_FB_PROJECT_ID: process.env.FB_PROJECT_ID || process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-        NEXT_PUBLIC_FB_APP_ID: process.env.FB_APP_ID || process.env.NEXT_PUBLIC_FB_APP_ID,
-        NEXT_PUBLIC_FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID || process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
