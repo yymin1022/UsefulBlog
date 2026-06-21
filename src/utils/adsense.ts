034267cff5ff7b3d940823926a1d59f215d5452f
yymin1022 <yymin1022@gmail.com>
2026-06-21 23:11:23 +0900

    Update adsense.ts : Add AdSense client and slot ID configurations

---
+++ src/utils/adsense.ts
@@ -0,0 +1,6 @@
+export const ADSENSE_CLIENT_ID = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || "ca-pub-7726147556907333";
+
+export const ADSENSE_SLOTS = {
+    POST_LIST: "6040016500",   // 포스팅 목록 광고 슬롯 ID
+    POST_DETAIL: "2567675357", // 포스트 디테일 광고 슬롯 ID
+};
