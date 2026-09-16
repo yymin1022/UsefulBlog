f4cfc34542dc923b47360e0520fe9e9615681b03
yymin1022 <yymin1022@gmail.com>
2026-09-16 22:48:00 +0900

    Update Project : Exclude s route from robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
-            disallow: ["/api/", "/getPostData", "/getPostImage", "/getPostList"],
+            disallow: ["/api/", "/getPostData", "/getPostImage", "/getPostList", "/s/"],
         },
         sitemap: `${SITE_URL}/sitemap.xml`,
     };
